6883ecefa6ebaaca6d3b8e89146a2fc6433f847c
Harsh Lakhanpal <harsh.lakhanpal3@gmail.com>
2023-10-26 10:12:45 +0530

    Bubble Game 1

--- Bubble Game/JS/index.js
+++ Bubble Game/JS/index.js
@@ -1,9 +1,10 @@
 var pbtm = document.querySelector(".pbtm");
+var time = document.querySelector(".time");
 
 function makeBubble() {
   var clutter = "";
 
-  for (i = 1; i <= 152; i++) {
+  for (i = 1; i <= 120; i++) {
     var x = Math.floor(Math.random() * 10);
     clutter += `<div class="bubble">${x}</div>`;
   }
@@ -11,4 +12,18 @@ function makeBubble() {
   pbtm.innerHTML = clutter;
 }
 
+function runTimer() {
+  var i = time.innerHTML;
+  setInterval(() => {
+    if (i > 0) {
+      i--;
+      time.textContent = i;
+    } else {
+      i = 0;
+      time.style.backgroundColor = "red";
+    }
+  }, 1000);
+}
+
+runTimer();
 makeBubble();
