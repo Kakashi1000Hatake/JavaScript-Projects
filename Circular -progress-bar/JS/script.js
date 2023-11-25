c7cfbbe64deedd7ccc1506e49f1fa56fce3f8de4
Harsh Lakhanpal <harsh.lakhanpal3@gmail.com>
2023-11-25 20:39:02 +0530

    New Porjects Added

---
+++ Circular -progress-bar/JS/script.js
@@ -0,0 +1,20 @@
+let progressbar = document.querySelector(".circular-progress");
+
+let valueContainer = document.querySelector(".value-container");
+
+let progressValue = 0;
+let progressEndValue = 65;
+let speed = 10;
+
+progressEndValue = prompt("Enter a value")
+
+let progress = setInterval(() => {
+    progressValue++;
+
+    valueContainer.textContent = `${progressValue}%`;
+    progressbar.style.background = `conic-gradient(#4d5bf9 ${progressValue * 3.6}deg, #cadcff ${progressValue * 3.6}deg )`
+    if(progressValue == progressEndValue){
+        clearInterval(progress);
+    }
+
+},speed)
---
+++ Quotes-generator/JS/script.js
@@ -0,0 +1,13 @@
+var quotes = require("quotes-goodreads");
+
+// var randomQuotes = quotes.getRandomStringQuote();
+console.log(quotes.getRandomStringQuote());
+
+var h1 = document.querySelector(".box h1");
+var btn = document.querySelector(".box button");
+
+console.log(randomQuotes);
+
+// btn.addEventListener("click", () => {
+//   console.log(randomQuotes);
+// });
